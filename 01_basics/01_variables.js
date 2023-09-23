c05f2a4294049ae0cc8ebe3e26e837135376c463
Suryakant9984 <112506176+Suryakant9984@users.noreply.github.com>
2023-09-23 14:29:19 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1356;
+let accountEmail = "srya@gmail.com"
+var accountPassword = "1234"
+accountCity = "LKO"
+let state;
+
+
+// accountEmail = "suryakant@gamil.com"
+// accountPassword = "12356"
+// accountCity = "Mumbai" 
+//  state = null 
+console.log([accountEmail, accountPassword, accountCity, state])
